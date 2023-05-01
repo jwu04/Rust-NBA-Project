341e67eebcb14be2e6bf1ab565d7d3afc7b96798
Jonathan Wu <wu.jonathan09@gmail.com>
2023-05-01 19:49:58 -0400

    added code to read csv files

--- project/src/main.rs
+++ project/src/main.rs
@@ -1,3 +1,35 @@
+use std::error::Error;
+use std::fs;
+use csv;
 fn main() {
-    println!("Hello, world!");
+    let paths = fs::read_dir("../data").unwrap();
+
+    for path in paths {
+        let file = path.unwrap().path().display().to_string();
+        
+        if let Err(e) = read_file(&file) {
+            eprintln!("{}", e);
+        } 
+    }
 }
+
+fn read_file(path: &str) -> Result<(), Box<dyn Error>> {
+    let mut rdr = csv::Reader::from_path(path)?;
+
+    let hdr = rdr.headers()?;
+    println!("{:?}", hdr);
+
+    for result in rdr.records() {
+        let record = result?;
+        println!("{:?}", record);
+    }
+
+    Ok(())
+}
+
+// fn read_file() -> String {
+//     let mut file = File::open("../../data/teams.csv").expect("Could not open file");
+//     let mut contents = String::new();
+//     file.read_to_string(&mut contents).expect("Could not read file");
+//     return contents;
+// }
